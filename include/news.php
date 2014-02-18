76ed39cc4901206e08ba98d16f99395b3682e249
LuDoe <ludovic.haefflinger@gmail.com>
2014-02-18 16:10:03 +0100

    Correctif lien projket

--- include/news.php
+++ include/news.php
@@ -1,6 +1,6 @@
 		<ul id="categories" class="section">
 		<?php
-<<<<<<< HEAD
+
 
 		$rq= "select `id`, `date_debut`, `nom`, `objectifs` from `projets` order by `date_debut` desc limit 3";
 		$table= $connection->query($rq);
@@ -12,18 +12,6 @@
 				<a href="detail_projet.php?projet='.$row[0].'">'.$row[1].'</a>"
 			</p>
 			</li>';
-
-		
-
-=======
-		$rq= "SELECT date_debut, nom, objectifs FROM projets ORDER BY date_debut DESC LIMIT 3";
-		$table= $connection->query($rq);
-                
-		while( $row=$table->fetch_row())
-			{
-			echo "<li><h5>".formatDate($row[0])."</h5>";
-			echo "<p>".$row[1]."</p></li>";
->>>>>>> 92746776c30bb9cb5dd7a20cca42cd2d314a8adb
 			}			
 		?>
 			<!--li>
