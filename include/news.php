2b6559f3de8a457392bcf095664443f6cbe9963f
LuDoe <ludovic.haefflinger@gmail.com>
2014-02-18 14:46:44 +0100

    Nouvelle zone News

--- include/news.php
+++ include/news.php
@@ -1,5 +1,14 @@
 		<ul id="categories" class="section">
-			<li>
+		<?php
+		$rq= "select `date_debut`, `nom`, `objectifs` from `projets` order by `date_debut` desc limit 3";
+		$table= $connection->query($rq);
+		while( $row=$table-> fetch_row())
+			{
+			echo "<li><h5>".formatDate($row[0])."</h5>";
+			echo "<p>".$row[1]."</p></li>";
+			}			
+		?>
+			<!--li>
 					<img src="images/gear.png" alt="Img" height="53" width="60">
 					<h3>Froibrush Title One</h3>
 					<p>
@@ -22,7 +31,7 @@
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu ante in enim hendrerit lobortis sit amet eget magna. Fusce tincidunt, dolor ut tempor
 					</p>
 					<a href="index.html" class="select">Select</a>
-				</li>
+				</li-->
 			</ul>
 			<!-- /#categories -->
 		</div>
