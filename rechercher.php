1d4c737ba1f57172341f4eb5b6e6a03ff0a41ec9
LuDoe <ludovic.haefflinger@gmail.com>
2014-02-19 08:39:55 +0100

    Navigation 2 (le retour)

--- rechercher.php
+++ rechercher.php
@@ -3,6 +3,7 @@ require_once("include/header_meta.php");
 require_once("include/menu.php");
 require_once("include/banniere.php");
 require_once("include/connect.php");
+$connection = Connect::getInstance();
 
 print('<h1>Zone de rechercher</h1>');
 
