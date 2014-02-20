f743241f05284640b5e5381b62ed9561c4249da2
BaptisteDerouin <baptiste.derouin@free.fr>
2014-02-20 12:16:19 +0100

    YES

--- admintest/modele/Modele.Personne.php
+++ admintest/modele/Modele.Personne.php
@@ -44,12 +44,8 @@ class Personne implements DAO{
         $this->metier = $row[5];
     }
 
-<<<<<<< HEAD:admintest/modele/Modele.Personne.php
 
     public static function listerTout($mysqli) {
-=======
-    static public function listerTout($mysqli) {
->>>>>>> 3ddc73670536801373c01b5e0554d870318142a6:admintest/Modele/Modele.Personne.php
 
         $lesPersonnes = array();
         $sql = "SELECT * FROM personnes ";
