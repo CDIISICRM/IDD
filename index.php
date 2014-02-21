94fb6960275ddb97e29cb341a18e0aac91fe3d40
FabriceLuneau <fabrice.luneau76@gmail.com>
2014-02-21 09:03:58 +0100

    coucou

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ include("admintest/Modele/Modele.Personne.php");
 ?>
 
 <?php
+//coucou
  $connection = Connect::getInstance();
 	$req = "SELECT titre, texte, img, extension FROM contenus WHERE id=1";
 	$res = $connection->query($req); 
