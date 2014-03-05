853aa69f97ae18da81020584b7887bea72d0d191
LuDoe <ludovic.haefflinger@gmail.com>
2014-03-05 16:02:54 +0100

    changement de menu

--- include/menu.php
+++ include/menu.php
@@ -1,3 +1,36 @@
+<?php
+// print('<pre>');
+// print_r($_SERVER);
+// print('</pre>');
+
+$accueilSelected = '';
+$projetSelected = '';
+$presseSelected = '';
+$contactSelected = '';
+			
+if($_SERVER['SCRIPT_NAME'] == '/detail_projet.php')
+	{
+	$projetSelected = ' class="selected"';
+	}
+else
+	{
+	switch($_SERVER['REQUEST_URI'])
+		{
+		case '/projets.php?page=1&pagination=5':
+			$projetSelected = ' class="selected"';
+			break;
+		case '/index.php?controller=Presse&action=Presse_info':
+			$presseSelected = ' class="selected"';
+			break;
+		case '/index.php?controller=Contact&action=FormulaireContact':
+			$contactSelected = ' class="selected"';
+			break;
+		default:
+			$accueilSelected = ' class="selected"';
+		}
+	}
+?>
+
 <body>
 	<div id="page">
 		<div id="header">
@@ -9,16 +42,16 @@
 				<input type="submit" value="Go">
 			</form>
 			<ul id="navigation">
-				<li class="selected">
+				<li<?php echo $accueilSelected; ?>>
 					<a href="index.php">Accueil & Présentation</a>
 				</li>
-				<li>
+				<li<?php echo $projetSelected; ?>>
                     <a href="projets.php?page=1&pagination=5">Réalisations</a>
 				</li>
-				<li>
+				<li<?php echo $presseSelected; ?>>
 					<a href="index.php?controller=Presse&action=Presse_info">Presse et partenaires</a>
 				</li>
-				<li>
+				<li<?php echo $contactSelected; ?>>
 					<a href="index.php?controller=Contact&action=FormulaireContact">Contact</a>
 				</li>
 			</ul>
