e11a53b54e2523a6a31d87171650e53edf3a2e55
LuDoe <ludovic.haefflinger@gmail.com>
2014-03-05 12:35:47 +0100

    Gestion des partenaires validé

--- admintest/modifier_partenaire.php
+++ admintest/modifier_partenaire.php
@@ -1,120 +1,98 @@
 <?php
-
 require_once('header.php');
-//require_once('../include/connect.php'); 
 require_once('modele/Modele.Partenaire.php');
-require("../include/class_upload.php");
-?>
+require_once("../include/class_upload.php");
 
-
-
-<?php
 $mysqli = Connect::getInstance();
 
 $getid=$_GET['id'];
-//$Partenaire = new Partenaire(NULL,NULL,NULL,NULL,$getid);
 
 $Partenaire=Partenaire::chercher($mysqli, $_GET['id']);
-/*echo"getid=".$getid;*/
+
 if(isset($_POST['valider'] ))
-{ 
+	{ 
 	$oldLogo=$Partenaire->logo;
 	if (($_FILES['fichierLogo']['name']!=''))//on change le logo
-	{
+		{
 		$obj = new upload('../images/','fichierLogo');
 		$obj->cl_taille_maxi = 49000000;
 		$obj->cl_extensions = array('.gif','.jpg','.png');
 		if (!$obj->uploadFichier('aleatoire'))
-	   {
-			  // affichage d'une erreur en cas d'echec
-			  echo $obj->affichageErreur();
-	   }
-	   else
-	   {
+			{
+			// affichage d'une erreur en cas d'echec
+			echo $obj->affichageErreur();
+			}
+		else
+			{
 			if (is_file('../images/'.$oldLogo)) unlink('../images/'.$oldLogo);
 			$partenaire=new Partenaire($_POST['nom'],$_POST['site'],$obj->cGetNameFileFinal(true),$_POST['sygle'],$_GET['id']);
-	   }
-	}
-	 else//on conserve l'ancien logo
-	 {
+			}
+		}
+	 else
+		{
+		//on conserve l'ancien logo
 		 $partenaire=new Partenaire($_POST['nom'],$_POST['site'],$oldLogo,$_POST['sygle'],$_GET['id']);
-	 }
-	 
+		}
 	$partenaire->modifier($mysqli);
 	echo "<center><strong>Les modifications ont bien été enregistrées.</strong></center>";
 	echo '<meta http-equiv="refresh" content="2;URL=listepartenaire.php">'; 
-	
-}
+	}	
 else
-{
+	{
+	echo '
+	<form action="modifier_partenaire.php?id='.$getid.'" method="post" name="form1" enctype="multipart/form-data">
+		<table align="center" id="formPartenaire">
+			<caption>Modifier un Partenaire d"un projet </caption>
+			<tr>
+				<td class="label">
+					<span>Nom</span>
+				</td>
+				<td>
+					<input type="text" name="nom" value="'.$Partenaire->nom.'" size="40" />
+				</td>
+			</tr>
+			<tr>
+				<td class="label">
+					<span>Site Internet</span>
+				</td>
+				<td>
+					<input type="text" name="site" value="'.$Partenaire->siteInternet.'" size="40" />
+				</td>
+			</tr>
+			<tr>
+				<td class="label">
+					<span>Sigle</span>
+				</td>
+				<td>
+					<input type="text" name="sygle" value="'.$Partenaire->sygle.'" size="40" />
+				</td>
+			</tr>
+			<tr>
+				<td class="label">
+					<span>Logo actuel</span>
+				</td>
+				<td>
+					<img src="../images/'.$Partenaire->logo.'" alt="Logo" height="150px" />
+				</td>
+			</tr>
+			<tr>
+				<td class="label">
+					<span>Télécharger le logo</span>
+				</td>
+				<td>
+					<input type="file" name="fichierLogo" value="" placeholder="Modifier le fichier ici"/>
+				</td>
+			</tr>
+			<tr>
+				<td class="label" colspan="2">
+					<input type="button" value="Retour" onClick="Javascript: window.history.back();"/>
+					&nbsp;&nbsp;
+					<input type="submit" name="valider" value="Modifier" /> 
+				</td>
+			</tr>
+		</table>
+	</form>';
+	}
 
-	
-/*$rq="select date,titre,texte from evenements where id=".$_GET['id'];											
-$rquery=@mysql_db_query($base,$rq);  
-$conteneur=mysql_fetch_row($rquery);
-$date=datefr($conteneur['0']);
-$titre=stripslashes($conteneur['1']);
-$texte=stripslashes($conteneur['2']);*/
-
-
-
-
-echo"<table align='center'>
-<caption>Modifier un Partenaire d'un projet </caption>
-<form action='modifier_partenaire.php?id=$getid' method='post' name='form1' enctype='multipart/form-data'>
-<tr>
-<td align='right'><font color='#663300' face='Arial, Helvetica, sans-serif' size='+1'>NOM</font></td>
-<td align='left'>
-<input type='text' name='nom' value=\"".$Partenaire->nom."\" size='40' />
-
-
-<tr>
-<td align='right'><font color='#663300' face='Arial, Helvetica, sans-serif' size='+1'>Site Internet</font></td>
-<td align='left'>
-<input type='text' name='site' value=\"".$Partenaire->siteInternet."\" size='40' />
-
-</td>
-</tr>
-
-<tr>
-<td align='right'><font color='#663300' face='Arial, Helvetica, sans-serif' size='+1'>SIGLE</font></td>
-<td align='left'>
-<input type='text' name='sygle' value=\"".$Partenaire->sygle."\" size='40' />
- 
-</td>
-</tr>
-<tr>
-<td align='right'>
-	<font color='#663300' face='Arial, Helvetica, sans-serif' size='+1'>Logo actuel
-</td>
-<td align='left'>
-	<img src='../images/".$Partenaire->logo."' alt='Logo' height='150px' />
-</td>
-</tr>
-<tr>
-	  <td align='right'>
-		<font color='#663300' face='Arial, Helvetica, sans-serif' size='+1'>Telecharger le logo
-	  </td>
-	  <td align='left'>
-		  <input type='file' name='fichierLogo' value='' placeholder='Modifier le fichier ici'/>
-	  </td>
-</tr>
-
-
-<tr>
-<td>
-</td>
-<td align='left'>
-<input type='submit' name=\"valider\" value='modifier' /> 
-</td>
-</tr>
-
-</form>  
-</table>";
-
-}
-?>
-
-<?php
 include('footer.php');
 ?>
