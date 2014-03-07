4155f7991dc894b17ef4daf2b912df3f5c07bfbb
rachid2014 <ahajri.r@sfr.fr>
2014-03-07 11:23:26 +0100

    MIS Model.article / modifier_article / ajouter_article

---
+++ admin_gestion/listearticles.php
@@ -0,0 +1,55 @@
+<?php 
+
+include ('modele/Modele.DAO.php');
+require_once('header.php');
+include ("modele/Modele.Presse.php");
+// include("../include/connect.php");
+
+
+$connection = Connect::getInstance();
+$array = Presse::listerTout($connection);
+
+/*$partenaire= new Partenaire(NULL,NULL,NULL,NULL);
+
+$array= $partenaire->listerTout($connection);*/
+
+echo '<table border="1" cellpadding="5" cellspacing="5">';
+echo "<tr><td><b>Titre</b></td>
+			<td><b>Source</b></td>
+			<td><b>Auteur</b></td>
+			<td><b>Lien</b></td>
+			<td><b>Date Parution</b></td>
+	<td></td></tr>";
+
+for($i=0; $i<count($array);$i++  ){
+	echo"<tr> ";
+	echo"<td>".$array[$i]->titre."  </td> ";
+echo"<td>".$array[$i]->source."  </td> ";
+echo"<td>".$array[$i]->auteur."  </td> ";
+echo"<td>".$array[$i]->lien."  </td> ";
+echo"<td>".$array[$i]->dateParution."  </td> ";
+
+	echo"</tr> ";
+	}
+	
+	
+	echo"</table> ";
+
+
+
+echo'<a href="ajouter_article.php" target="_self"> <input type="button" value="Ajouter un article"></a>';
+
+
+
+
+
+
+
+
+
+require_once('footer.php');
+
+
+
+
+?>
