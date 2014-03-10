96e27023c689e5392bfaf78e7bf642b9e629e060
rachid2014 <ahajri.r@sfr.fr>
2014-03-10 14:54:01 +0100

    front end articles

--- include/menu.php
+++ include/menu.php
@@ -34,7 +34,7 @@ else
                     <a href="index.php?controller=ControlleurProjet&action=AfficherTousProjets&page=1&pagination=5">Réalisations</a>
 				</li>
 				<li<?php echo $presseSelected; ?>>
-					<a href="index.php?controller=Presse&action=Presse_info">Presse</a>
+					<a href="index.php?controller=Articles&action=Presse_info">Presse</a>
 				</li>
 				<li<?php echo $contactSelected; ?>>
 					<a href="index.php?controller=Contact&action=FormulaireContact">Contact</a>
