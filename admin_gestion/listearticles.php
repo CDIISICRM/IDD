c2f14a2b9d76aa066d8b0390ff298ad69118a70c
LuDoe <ludovic.haefflinger@gmail.com>
2014-03-10 16:10:34 +0100

    Modification mineur

--- admin_gestion/listearticles.php
+++ admin_gestion/listearticles.php
@@ -12,32 +12,36 @@ $array = Presse::listerTout($connection);
 /*$partenaire= new Partenaire(NULL,NULL,NULL,NULL);
 
 $array= $partenaire->listerTout($connection);*/
-
-echo '<table border="1" cellpadding="5" cellspacing="5">';
-echo "<tr><td><b>Titre</b></td>
+// var_dump($array);
+if($array)
+	{
+	echo '<table border="1" cellpadding="5" cellspacing="5">';
+	echo "<tr><td><b>Titre</b></td>
 			<td><b>Source</b></td>
 			<td><b>Auteur</b></td>
 			<td><b>Lien</b></td>
 			<td><b>pdf</b></td>
 			<td><b>Date Parution</b></td>
 	<td></td></tr>";
+	for($i=0; $i<count($array);$i++  ){
+		echo"<tr> ";
+		echo"<td>".$array[$i]->titre."  </td> ";
+		echo"<td>".$array[$i]->source."  </td> ";
+		echo"<td>".$array[$i]->auteur."  </td> ";
+		echo"<td>".$array[$i]->lien."  </td> ";
+		echo"<td>".$array[$i]->pdf."</td>";
+		echo"<td>".$array[$i]->dateParution."  </td> ";
 
-for($i=0; $i<count($array);$i++  ){
-	echo"<tr> ";
-	echo"<td>".$array[$i]->titre."  </td> ";
-	echo"<td>".$array[$i]->source."  </td> ";
-	echo"<td>".$array[$i]->auteur."  </td> ";
-	echo"<td>".$array[$i]->lien."  </td> ";
-	echo"<td>".$array[$i]->pdf."</td>";
-	echo"<td>".$array[$i]->dateParution."  </td> ";
-
-	echo'<td> <a href=modifier_article.php?id='.$array[$i]->id.' target="_self">  modifier </a></td> ';
-	echo'<td> <a href=supprimer_article.php?id='.$array[$i]->id.' target="_self">  supprimer </a></td> ';
-	echo"</tr> ";
+		echo'<td> <a href=modifier_article.php?id='.$array[$i]->id.' target="_self">  modifier </a></td> ';
+		echo'<td> <a href=supprimer_article.php?id='.$array[$i]->id.' target="_self">  supprimer </a></td> ';
+		echo"</tr> ";
+		}
+		
+		
+		echo"</table> ";
 	}
-	
-	
-	echo"</table> ";
+else
+	echo '<center><p>Il n\'y a pas d\'article pour le moment</p></center>';
 
 
 
